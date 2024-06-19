897aa77715adf100089ec03d6696b80213282f72
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-06-19 06:26:11 +0530

    First Commit - Stack Using array

--- stack.py
+++ stack.py
@@ -2,6 +2,15 @@ class MyStack:
     def __init__(self) -> None:
         self._arr = []
 
+    def __len__(self):
+        return len(self._arr)
+    
+    def __str__(self) -> str:
+        return f'MyStack({self._arr})'
+    
+    def __repr__(self) -> str:
+        return str(self)
+    
     def push(self,item):
         self._arr.append(item)
 
@@ -13,3 +22,12 @@ class MyStack:
     
     def peek(self):
         return self._arr[-1]
+    
+if __name__=='__main__':
+    stack = MyStack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    print(stack)
+    print(stack.pop())
+    print(stack)
